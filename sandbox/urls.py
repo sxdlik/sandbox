5bcc23fbc947dd51dca012131574a8af12d02781
sxdlik <sxdlik@163.com>
2020-03-19 18:12:44 +0800

    项目初始配置，静态文件，媒体文件，中文化

---
+++ sandbox/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, re_path
+from django.conf import settings
+from django.views.static import serve
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
